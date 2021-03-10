4a19a67fb682a1ef75b3c8efbdaafffcbdee27ba
Vendicated <vendicated@riseup.net>
2021-03-10 03:11:07 +0100

    Minor improvements

--- script.js
+++ script.js
@@ -3,53 +3,15 @@ let birthday;
 
 // This is used so I can return fake dates for testing purposes :)
 function getDate() {
-  /* const d = new Date();
-  return new Date(
-    2021,
-    07,
-    22,
-    d.getHours(),
-    d.getMinutes() + 36,
-    d.getSeconds(),
-    15,
-    d.getMilliseconds()
-  ); */
-
   return new Date();
-}
-
-const now = getDate();
-const afterBirthday =
-  now.getMonth() > 7 || (now.getMonth() === 7 && now.getDate() > 22);
-
-const futureAge = afterBirthday
-  ? now.getFullYear() - 2001
-  : now.getFullYear() - 2002;
-
-function celebrate() {
-  document.getElementById(
-    "birthday"
-  ).innerHTML = `IT'S MY BIRTHDAY TODAY! I'M ${futureAge} NOW :DDDD`;
-  fetch("https://cdn.jsdelivr.net/gh/mathusummut/confetti.js/confetti.min.js")
-    .then((res) => res.text())
-    .then((text) => {
-      eval(text);
-      confetti.start();
-    });
-}
-
-function updateTimer(date) {
-  document.getElementById(
-    "birthday"
-  ).innerHTML = `I turn ${futureAge} in ${formatTime(date)}`;
 }
 
 function pluralise(i, name) {
   return i === 1 ? `${i} ${name}` : `${i} ${name}s`;
 }
 
-function formatTime(date) {
-  const duration = birthday - date.getTime();
+function formatTime(d) {
+  const duration = birthday - d.getTime();
 
   let seconds = parseInt((duration / 1000) % 60),
     minutes = parseInt((duration / (1000 * 60)) % 60),
@@ -62,20 +24,41 @@ function formatTime(date) {
   )} and ${pluralise(seconds, "second")}`;
 }
 
+function celebrate() {
+  document.getElementById(
+    "birthday"
+  ).innerHTML = `IT'S MY BIRTHDAY TODAY! I'M ${futureAge} NOW :DDDD`;
+  confetti.start();
+}
+
+function updateTimer(d) {
+  document.getElementById(
+    "birthday"
+  ).innerHTML = `I turn ${futureAge} in ${formatTime(d)}`;
+}
+
+function tick() {
+  const d = getDate();
+  if (d.getMonth() === 7 && d.getDate() === 22) {
+    clearInterval(interval);
+    celebrate();
+  } else {
+    updateTimer(d);
+  }
+}
+
+const now = getDate();
+const afterBirthday =
+  now.getMonth() > 7 || (now.getMonth() === 7 && now.getDate() > 22);
+
+const futureAge = afterBirthday
+  ? now.getFullYear() - 2001
+  : now.getFullYear() - 2002;
+
 if (now.getMonth() > 7 || (now.getMonth() === 7 && now.getDate() > 22)) {
   birthday = new Date(now.getFullYear() + 1, 7, 22).getTime();
 } else {
   birthday = new Date(now.getFullYear(), 7, 22).getTime();
-}
-
-function tick() {
-  const date = getDate();
-  if (date.getMonth() === 7 && date.getDate() === 22) {
-    clearInterval(interval);
-    celebrate();
-  } else {
-    updateTimer(date);
-  }
 }
 
 window.onload = () => {
