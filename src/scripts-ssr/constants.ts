9bac1b2afebab586be64dd84dc3684381ea09c22
Vendicated <vendicated@riseup.net>
2022-12-31 23:49:27 +0100

    meta stuff

---
+++ src/scripts-ssr/constants.ts
@@ -0,0 +1,4 @@
+export const AUTHOR = "Vendicated";
+export const AUTHOR_URL = "https://github.com/Vendicated";
+export const ICON = "https://github.com/Vendicated.png?size=256";
+export const THEME_COLOUR = "#dd7878";
