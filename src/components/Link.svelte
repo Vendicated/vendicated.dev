2d0851461de1ab14ec2698eb7a673d29b12c92a2
Vendicated <vendicated@riseup.net>
2023-01-09 19:33:54 +0100

    Some fixes
    Add everyone's favourite cat

---
+++ src/components/Link.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    // the only reason this file is Svelte is because Astro treats whitespace after inline elements
+    // like "a" as desired, meaning the newline between a and style is preserved
+
+    export let href: string;
+    export let title: string | undefined;
+    let className: string | undefined;
+    export { className as class };
+</script>
+
+<a class={className} {href} {title}><slot /></a>
+
+<style>
+    a {
+        text-decoration: underline;
+        color: var(--color);
+
+        transition: color 0.2s ease;
+    }
+
+    a:hover,
+    a:focus {
+        color: var(--accent);
+        text-decoration-thickness: 2px;
+        outline: none;
+    }
+</style>
