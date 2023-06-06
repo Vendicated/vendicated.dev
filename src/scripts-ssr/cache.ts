818db1840bfca00f340ab1ee576059c79ab9d74c
V <vendicated@riseup.net>
2023-06-07 01:20:50 +0200

    Cache only in prod

--- src/scripts-ssr/cache.ts
+++ src/scripts-ssr/cache.ts
@@ -4,10 +4,11 @@ export const HOURS = 60 * MINUTES;
 export const DAYS = 24 * HOURS;
 
 export function cache<R extends { headers: Headers; }>(res: R, seconds: number): R {
-    res.headers.set(
-        "Cache-Control",
-        `public, max-age=${seconds}, s-maxage=${seconds}`
-    );
+    if (import.meta.env.PROD)
+        res.headers.set(
+            "Cache-Control",
+            `public, max-age=${seconds}, s-maxage=${seconds}`
+        );
 
     return res;
 }
