eb0476392aea22c065a6b461c5ced58ff4cf0d89
vee <vee@noreply.codeberg.org>
2024-01-09 17:18:48 +0000

    Update src/scripts-ssr/constants.ts

--- src/scripts-ssr/constants.ts
+++ src/scripts-ssr/constants.ts
@@ -2,6 +2,6 @@ export const AUTHOR = "Vendicated";
 export const AUTHOR_URL = "https://github.com/Vendicated";
 export const ICON = "https://github.com/Vendicated.png?size=256";
 export const THEME_COLOUR = "#dd7878";
-export const SOURCE_CODE_URL = "https://codeberg.org/Ven/vendicated.dev";
+export const SOURCE_CODE_URL = "https://codeberg.org/vee/vendicated.dev";
 
 export const ERR_NOT_FOUND = new Response("Not Found", { status: 404 });
