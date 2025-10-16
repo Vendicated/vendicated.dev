03a4c359e6a346ad3d2fba82c52ff96b360c5de5
Vendicated <vendicated@riseup.net>
2025-10-16 23:37:56 +0200

    remake theme toggle without need for javascript

--- src/scripts/themeRuntime.ts
+++ src/scripts/themeRuntime.ts
@@ -1,11 +1,27 @@
+import "./polyfills";
+
+export type Theme = "light" | "dark" | "system";
+
 export function getCurrentTheme() {
     let currentTheme = localStorage.getItem("theme");
-    if (!currentTheme) {
+    if (!currentTheme || currentTheme === "system") {
         currentTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
             ? "dark"
             : "light";
-        localStorage.setItem("theme", currentTheme);
     }
 
     return currentTheme;
 }
+
+export function setTheme(theme: Theme) {
+    if (theme === "system") {
+        localStorage.removeItem("theme");
+    } else {
+        localStorage.setItem("theme", theme);
+    }
+
+    document.dispatchEvent(new Event("themechange"));
+    console.log("Switched theme to", theme);
+}
+
+globalThis.setTheme = setTheme;
